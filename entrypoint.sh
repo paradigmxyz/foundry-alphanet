f10322f2aa19b5ce914a8f5bba8968f8074a42a6
Federico Gimenez <federico.gimenez@gmail.com>
2024-03-10 12:03:27 +0100

    named args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-FOUNDRY_COMMAND="$1"
-SOLC_PATH="--use /usr/local/bin/solc"
-FOUNDRY_DIRECTORY="--root $2"
-FOUNDRY_SCRIPT="$3"
+main() {
+    local FOUNDRY_COMMAND=""
+    local FOUNDRY_DIRECTORY=""
+    local FOUNDRY_SCRIPT=""
+    local SOLC_PATH="--use /usr/local/bin/solc"
 
-if [ "$FOUNDRY_COMMAND" = "script" ] && [ -z "$FOUNDRY_SCRIPT" ]; then
-  echo "Error: 'foundry-script' is required when 'foundry-command' is 'script'"
-  exit 1
-fi
+    # Parse named arguments
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            --foundry-command) FOUNDRY_COMMAND="$2"; shift ;;
+            --foundry-directory) FOUNDRY_DIRECTORY="--root $2"; shift ;;
+            --foundry-script) FOUNDRY_SCRIPT="$2"; shift ;;
+            *) echo "Unknown parameter passed: $1"; exit 1 ;;
+        esac
+        shift
+    done
 
-if [ "$FOUNDRY_COMMAND" = "script" ]; then
-  exec forge script $FOUNDRY_SCRIPT $SOLC_PATH $FOUNDRY_DIRECTORY --fork-url http://localhost:8545 --broadcast -vvvv
-else
-  exec forge $FOUNDRY_COMMAND $SOLC_PATH $FOUNDRY_DIRECTORY
-fi
+    if [ "$FOUNDRY_COMMAND" = "script" ] && [ -z "$FOUNDRY_SCRIPT" ]; then
+        echo "Error: 'foundry-script' is required when 'foundry-command' is 'script'"
+        exit 1
+    fi
+
+    if [ "$FOUNDRY_COMMAND" = "script" ]; then
+        exec forge script $FOUNDRY_SCRIPT $SOLC_PATH $FOUNDRY_DIRECTORY --fork-url http://localhost:8545 --broadcast -vvvv
+    else
+        exec forge $FOUNDRY_COMMAND $SOLC_PATH $FOUNDRY_DIRECTORY
+    fi
+}
